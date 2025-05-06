a8896eced5345c62ae8e0ec5693a317bcbd4c861
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-05-06 15:43:00 -0400

    feat: add StyleString to decompose styled strings into cells

---
+++ styled.go
@@ -0,0 +1,273 @@
+package tv
+
+import (
+	"bytes"
+	"image/color"
+	"strings"
+
+	"github.com/charmbracelet/x/ansi"
+	"github.com/mattn/go-runewidth"
+	"github.com/rivo/uniseg"
+)
+
+// StyledString is a string that can be decomposed into a series of styled
+// lines and cells. It is used to disassemble a rendered string with ANSI
+// escape codes into a series of cells that can be used in a [Buffer].
+// A StyledString supports reading [ansi.SGR] and [ansi.Hyperlink] escape
+// codes.
+type StyledString struct {
+	// The width method used to calculate the width of each unicode character
+	// and grapheme cluster.
+	Method ansi.Method
+	// The buffer that contains the decomposed string.
+	Buffer *Buffer
+}
+
+// NewStyledString creates a new [StyledString] for the given method and styled
+// string. The method is used to calculate the width of each line.
+func NewStyledString(method ansi.Method, str string) *StyledString {
+	ss := new(StyledString)
+	ss.Method = method
+
+	var w, h int
+	lines := strings.Split(str, "\n")
+	h = len(lines)
+	for _, l := range lines {
+		width := method.StringWidth(l)
+		if width > w {
+			w = width
+		}
+	}
+
+	ss.Buffer = NewBuffer(w, h)
+	printString(ss.Buffer, method, 0, 0, ss.Buffer.Bounds(), str, true, "")
+	return ss
+}
+
+// printString draws a string starting at the given position.
+func printString[T []byte | string](
+	s *Buffer,
+	m ansi.Method,
+	x, y int,
+	bounds Rectangle, str T,
+	truncate bool, tail string,
+) {
+	p := ansi.GetParser()
+	defer ansi.PutParser(p)
+
+	var tailc Cell
+	if truncate && len(tail) > 0 {
+		if m == ansi.WcWidth {
+			for i, r := range tail {
+				if i == 0 {
+					tailc.Rune = r
+					// We only care about the first rune's width
+					tailc.Width = runewidth.RuneWidth(r)
+				} else {
+					if runewidth.RuneWidth(r) > 0 {
+						break
+					}
+					tailc.Comb = append(tailc.Comb, r)
+				}
+			}
+		} else {
+			g, _, w, _ := uniseg.FirstGraphemeClusterInString(tail, -1)
+			tailc.Width = w
+			for i, r := range g {
+				if i == 0 {
+					tailc.Rune = r
+				} else {
+					tailc.Comb = append(tailc.Comb, r)
+				}
+			}
+		}
+	}
+
+	decoder := ansi.DecodeSequenceWc[T]
+	if m == ansi.GraphemeWidth {
+		decoder = ansi.DecodeSequence[T]
+	}
+
+	var cell Cell
+	var style Style
+	var link Link
+	var state byte
+	for len(str) > 0 {
+		seq, width, n, newState := decoder(str, state, p)
+		switch width {
+		case 1, 2, 3, 4: // wide cells can go up to 4 cells wide
+			cell.Width += width
+			cell.Append([]rune(string(seq))...)
+
+			if !truncate && x+cell.Width > bounds.Max.X && y+1 < bounds.Max.Y {
+				// Wrap the string to the width of the window
+				x = bounds.Min.X
+				y++
+			}
+			if Pos(x, y).In(bounds) {
+				if truncate && tailc.Width > 0 && x+cell.Width > bounds.Max.X-tailc.Width {
+					// Truncate the string and append the tail if any.
+					cell := tailc
+					cell.Style = style
+					cell.Link = link
+					s.SetCell(x, y, cell.Clone())
+					x += tailc.Width
+				} else {
+					// Print the cell to the screen
+					cell.Style = style
+					cell.Link = link
+					s.SetCell(x, y, cell.Clone()) //nolint:errcheck
+					x += width
+				}
+			}
+
+			// String is too long for the line, truncate it.
+			// Make sure we reset the cell for the next iteration.
+			cell.Reset()
+		default:
+			// Valid sequences always have a non-zero Cmd.
+			// TODO: Handle cursor movement and other sequences
+			switch {
+			case ansi.HasCsiPrefix(seq) && p.Command() == 'm':
+				// SGR - Select Graphic Rendition
+				ReadStyle(p.Params(), &style)
+			case ansi.HasOscPrefix(seq) && p.Command() == 8:
+				// Hyperlinks
+				ReadLink(p.Data(), &link)
+			case ansi.Equal(seq, T("\n")):
+				y++
+			case ansi.Equal(seq, T("\r")):
+				x = bounds.Min.X
+			default:
+				cell.Append([]rune(string(seq))...)
+			}
+		}
+
+		// Advance the state and data
+		state = newState
+		str = str[n:]
+	}
+
+	// Make sure to set the last cell if it's not empty.
+	if !cell.Empty() {
+		s.SetCell(x, y, cell.Clone()) //nolint:errcheck
+		cell.Reset()
+	}
+}
+
+// ReadStyle reads a Select Graphic Rendition (SGR) escape sequences from a
+// list of parameters into pen.
+func ReadStyle(params ansi.Params, pen *Style) {
+	if len(params) == 0 {
+		pen.Reset()
+		return
+	}
+
+	for i := 0; i < len(params); i++ {
+		param, hasMore, _ := params.Param(i, 0)
+		switch param {
+		case 0: // Reset
+			pen.Reset()
+		case 1: // Bold
+			pen.Bold(true)
+		case 2: // Dim/Faint
+			pen.Faint(true)
+		case 3: // Italic
+			pen.Italic(true)
+		case 4: // Underline
+			nextParam, _, ok := params.Param(i+1, 0)
+			if hasMore && ok { // Only accept subparameters i.e. separated by ":"
+				switch nextParam {
+				case 0, 1, 2, 3, 4, 5:
+					i++
+					switch nextParam {
+					case 0: // No Underline
+						pen.UnderlineStyle(NoUnderline)
+					case 1: // Single Underline
+						pen.UnderlineStyle(SingleUnderline)
+					case 2: // Double Underline
+						pen.UnderlineStyle(DoubleUnderline)
+					case 3: // Curly Underline
+						pen.UnderlineStyle(CurlyUnderline)
+					case 4: // Dotted Underline
+						pen.UnderlineStyle(DottedUnderline)
+					case 5: // Dashed Underline
+						pen.UnderlineStyle(DashedUnderline)
+					}
+				}
+			} else {
+				// Single Underline
+				pen.UnderlineStyle(SingleUnderline)
+			}
+		case 5: // Slow Blink
+			pen.SlowBlink(true)
+		case 6: // Rapid Blink
+			pen.RapidBlink(true)
+		case 7: // Reverse
+			pen.Reverse(true)
+		case 8: // Conceal
+			pen.Conceal(true)
+		case 9: // Crossed-out/Strikethrough
+			pen.Strikethrough(true)
+		case 22: // Normal Intensity (not bold or faint)
+			pen.Bold(false).Faint(false)
+		case 23: // Not italic, not Fraktur
+			pen.Italic(false)
+		case 24: // Not underlined
+			pen.UnderlineStyle(NoUnderline)
+		case 25: // Blink off
+			pen.SlowBlink(false).RapidBlink(false)
+		case 27: // Positive (not reverse)
+			pen.Reverse(false)
+		case 28: // Reveal
+			pen.Conceal(false)
+		case 29: // Not crossed out
+			pen.Strikethrough(false)
+		case 30, 31, 32, 33, 34, 35, 36, 37: // Set foreground
+			pen.Foreground(ansi.Black + ansi.BasicColor(param-30)) //nolint:gosec
+		case 38: // Set foreground 256 or truecolor
+			var c color.Color
+			n := ansi.ReadStyleColor(params[i:], &c)
+			if n > 0 {
+				pen.Foreground(c)
+				i += n - 1
+			}
+		case 39: // Default foreground
+			pen.Foreground(nil)
+		case 40, 41, 42, 43, 44, 45, 46, 47: // Set background
+			pen.Background(ansi.Black + ansi.BasicColor(param-40)) //nolint:gosec
+		case 48: // Set background 256 or truecolor
+			var c color.Color
+			n := ansi.ReadStyleColor(params[i:], &c)
+			if n > 0 {
+				pen.Background(c)
+				i += n - 1
+			}
+		case 49: // Default Background
+			pen.Background(nil)
+		case 58: // Set underline color
+			var c color.Color
+			n := ansi.ReadStyleColor(params[i:], &c)
+			if n > 0 {
+				pen.Underline(c)
+				i += n - 1
+			}
+		case 59: // Default underline color
+			pen.Underline(nil)
+		case 90, 91, 92, 93, 94, 95, 96, 97: // Set bright foreground
+			pen.Foreground(ansi.BrightBlack + ansi.BasicColor(param-90)) //nolint:gosec
+		case 100, 101, 102, 103, 104, 105, 106, 107: // Set bright background
+			pen.Background(ansi.BrightBlack + ansi.BasicColor(param-100)) //nolint:gosec
+		}
+	}
+}
+
+// ReadLink reads a hyperlink escape sequence from a data buffer into link.
+func ReadLink(p []byte, link *Link) {
+	params := bytes.Split(p, []byte{';'})
+	if len(params) != 3 {
+		return
+	}
+	link.Params = string(params[1])
+	link.URL = string(params[2])
+}
