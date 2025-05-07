f0f1b8e3d9334fc6ec9e5193d7809fd6d7d8e7c4
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-05-07 17:08:32 -0400

    fix(widget): styledstring: clear area before drawing

--- widget/styledstring/styledstring.go
+++ widget/styledstring/styledstring.go
@@ -17,13 +17,14 @@ var _ tv.Widget = StyledString{}
 
 // Display implements tv.Widget.
 func (s StyledString) Display(buf *tv.Buffer, area tv.Rectangle) error {
+	// Clear the area before drawing.
+	buf.FillArea(nil, area)
+
 	var x, y int
 	for y = area.Min.Y; y < area.Max.Y; y++ {
 		for x = area.Min.X; x < area.Max.X; {
 			cell := s.Buffer.CellAt(x-area.Min.X, y-area.Min.Y)
 			if cell == nil {
-				// Fill nil cells with blank cells.
-				buf.SetCell(x, y, nil)
 				x++
 				continue
 			}
