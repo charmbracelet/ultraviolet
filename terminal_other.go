4bf6a65f99d72779215fd142d5fa820df0796d3e
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-05-14 16:17:18 -0400

    fix: js and wasm builds

--- terminal_other.go
+++ terminal_other.go
@@ -7,6 +7,10 @@ func (*Terminal) makeRaw() error {
 	return ErrPlatformNotSupported
 }
 
+func (*Terminal) getSize() (int, int, error) {
+	return 0, 0, ErrPlatformNotSupported
+}
+
 func (t *Terminal) optimizeMovements() {}
 
 func (*Terminal) enableWindowsMouse() error  { return ErrPlatformNotSupported }
