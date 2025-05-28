f8c53ec113ed5c0dd9915da38ebeff4d094dad23
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-05-28 15:40:10 -0400

    chore: remove widget.go

--- widget.go
+++
@@ -1,7 +0,0 @@
-package tv
-
-// Component represents a displayable component on a [Buffer].
-type Component interface {
-	// Display displays the component on the screen within the given area.
-	Display(buf *Buffer, area Rectangle) error
-}
