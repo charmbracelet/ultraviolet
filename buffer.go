7dc3a3fae07b8732ce1df432d6d37275b9032ebb
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-06-03 17:20:40 -0400

    feat: buffer: add methods for inserting and deleting lines and cells

--- buffer.go
+++ buffer.go
@@ -360,13 +360,13 @@ func (b *Buffer) Fill(c *Cell) {
 }
 
 // FillArea fills the buffer with the given cell and rectangle.
-func (b *Buffer) FillArea(c *Cell, rect Rectangle) {
+func (b *Buffer) FillArea(c *Cell, area Rectangle) {
 	cellWidth := 1
 	if c != nil && c.Width > 1 {
 		cellWidth = c.Width
 	}
-	for y := rect.Min.Y; y < rect.Max.Y; y++ {
-		for x := rect.Min.X; x < rect.Max.X; x += cellWidth {
+	for y := area.Min.Y; y < area.Max.Y; y++ {
+		for x := area.Min.X; x < area.Max.X; x += cellWidth {
 			b.SetCell(x, y, c)
 		}
 	}
@@ -379,22 +379,22 @@ func (b *Buffer) Clear() {
 
 // ClearArea clears the buffer with space cells within the specified
 // rectangles. Only cells within the rectangle's bounds are affected.
-func (b *Buffer) ClearArea(rect Rectangle) {
-	b.FillArea(nil, rect)
+func (b *Buffer) ClearArea(area Rectangle) {
+	b.FillArea(nil, area)
 }
 
 // CloneArea clones the area of the buffer within the specified rectangle. If
 // the area is out of bounds, it returns nil.
-func (b *Buffer) CloneArea(rect Rectangle) *Buffer {
+func (b *Buffer) CloneArea(area Rectangle) *Buffer {
 	bounds := b.Bounds()
-	if !rect.In(bounds) {
+	if !area.In(bounds) {
 		return nil
 	}
-	n := NewBuffer(rect.Dx(), rect.Dy())
-	for y := rect.Min.Y; y < rect.Max.Y; y++ {
-		for x := rect.Min.X; x < rect.Max.X; x++ {
+	n := NewBuffer(area.Dx(), area.Dy())
+	for y := area.Min.Y; y < area.Max.Y; y++ {
+		for x := area.Min.X; x < area.Max.X; x++ {
 			c := b.CellAt(x, y)
-			n.SetCell(x-rect.Min.X, y-rect.Min.Y, c)
+			n.SetCell(x-area.Min.X, y-area.Min.Y, c)
 		}
 	}
 	return n
@@ -426,6 +426,160 @@ func (b *Buffer) Draw(scr Screen, area Rectangle) {
 			}
 			scr.SetCell(x, y, c)
 		}
+	}
+}
+
+// InsertLine inserts n lines at the given line position, with the given
+// optional cell, within the specified rectangles. If no rectangles are
+// specified, it inserts lines in the entire buffer. Only cells within the
+// rectangle's horizontal bounds are affected. Lines are pushed out of the
+// rectangle bounds and lost. This follows terminal [ansi.IL] behavior.
+// It returns the pushed out lines.
+func (b *Buffer) InsertLine(y, n int, c *Cell) {
+	b.InsertLineArea(y, n, c, b.Bounds())
+}
+
+// InsertLineArea inserts new lines at the given line position, with the
+// given optional cell, within the rectangle bounds. Only cells within the
+// rectangle's horizontal bounds are affected. Lines are pushed out of the
+// rectangle bounds and lost. This follows terminal [ansi.IL] behavior.
+func (b *Buffer) InsertLineArea(y, n int, c *Cell, area Rectangle) {
+	if n <= 0 || y < area.Min.Y || y >= area.Max.Y || y >= b.Height() {
+		return
+	}
+
+	// Limit number of lines to insert to available space
+	if y+n > area.Max.Y {
+		n = area.Max.Y - y
+	}
+
+	// Move existing lines down within the bounds
+	for i := area.Max.Y - 1; i >= y+n; i-- {
+		for x := area.Min.X; x < area.Max.X; x++ {
+			// We don't need to clone c here because we're just moving lines down.
+			b.Lines[i][x] = b.Lines[i-n][x]
+		}
+	}
+
+	// Clear the newly inserted lines within bounds
+	for i := y; i < y+n; i++ {
+		for x := area.Min.X; x < area.Max.X; x++ {
+			b.SetCell(x, i, c)
+		}
+	}
+}
+
+// DeleteLineArea deletes lines at the given line position, with the given
+// optional cell, within the rectangle bounds. Only cells within the
+// rectangle's bounds are affected. Lines are shifted up within the bounds and
+// new blank lines are created at the bottom. This follows terminal [ansi.DL]
+// behavior.
+func (b *Buffer) DeleteLineArea(y, n int, c *Cell, area Rectangle) {
+	if n <= 0 || y < area.Min.Y || y >= area.Max.Y || y >= b.Height() {
+		return
+	}
+
+	// Limit deletion count to available space in scroll region
+	if n > area.Max.Y-y {
+		n = area.Max.Y - y
+	}
+
+	// Shift cells up within the bounds
+	for dst := y; dst < area.Max.Y-n; dst++ {
+		src := dst + n
+		for x := area.Min.X; x < area.Max.X; x++ {
+			// We don't need to clone c here because we're just moving cells up.
+			b.Lines[dst][x] = b.Lines[src][x]
+		}
+	}
+
+	// Fill the bottom n lines with blank cells
+	for i := area.Max.Y - n; i < area.Max.Y; i++ {
+		for x := area.Min.X; x < area.Max.X; x++ {
+			b.SetCell(x, i, c)
+		}
+	}
+}
+
+// DeleteLine deletes n lines at the given line position, with the given
+// optional cell, within the specified rectangles. If no rectangles are
+// specified, it deletes lines in the entire buffer.
+func (b *Buffer) DeleteLine(y, n int, c *Cell) {
+	b.DeleteLineArea(y, n, c, b.Bounds())
+}
+
+// InsertCell inserts new cells at the given position, with the given optional
+// cell, within the specified rectangles. If no rectangles are specified, it
+// inserts cells in the entire buffer. This follows terminal [ansi.ICH]
+// behavior.
+func (b *Buffer) InsertCell(x, y, n int, c *Cell) {
+	b.InsertCellArea(x, y, n, c, b.Bounds())
+}
+
+// InsertCellArea inserts new cells at the given position, with the given
+// optional cell, within the rectangle bounds. Only cells within the
+// rectangle's bounds are affected, following terminal [ansi.ICH] behavior.
+func (b *Buffer) InsertCellArea(x, y, n int, c *Cell, area Rectangle) {
+	if n <= 0 || y < area.Min.Y || y >= area.Max.Y || y >= b.Height() ||
+		x < area.Min.X || x >= area.Max.X || x >= b.Width() {
+		return
+	}
+
+	// Limit number of cells to insert to available space
+	if x+n > area.Max.X {
+		n = area.Max.X - x
+	}
+
+	// Move existing cells within rectangle bounds to the right
+	for i := area.Max.X - 1; i >= x+n && i-n >= area.Min.X; i-- {
+		// We don't need to clone c here because we're just moving cells to the
+		// right.
+		// b.lines[y][i] = b.lines[y][i-n]
+		b.Lines[y][i] = b.Lines[y][i-n]
+	}
+
+	// Clear the newly inserted cells within rectangle bounds
+	for i := x; i < x+n && i < area.Max.X; i++ {
+		b.SetCell(i, y, c)
+	}
+}
+
+// DeleteCell deletes cells at the given position, with the given optional
+// cell, within the specified rectangles. If no rectangles are specified, it
+// deletes cells in the entire buffer. This follows terminal [ansi.DCH]
+// behavior.
+func (b *Buffer) DeleteCell(x, y, n int, c *Cell) {
+	b.DeleteCellArea(x, y, n, c, b.Bounds())
+}
+
+// DeleteCellArea deletes cells at the given position, with the given
+// optional cell, within the rectangle bounds. Only cells within the
+// rectangle's bounds are affected, following terminal [ansi.DCH] behavior.
+func (b *Buffer) DeleteCellArea(x, y, n int, c *Cell, area Rectangle) {
+	if n <= 0 || y < area.Min.Y || y >= area.Max.Y || y >= b.Height() ||
+		x < area.Min.X || x >= area.Max.X || x >= b.Width() {
+		return
+	}
+
+	// Calculate how many positions we can actually delete
+	remainingCells := area.Max.X - x
+	if n > remainingCells {
+		n = remainingCells
+	}
+
+	// Shift the remaining cells to the left
+	for i := x; i < area.Max.X-n; i++ {
+		if i+n < area.Max.X {
+			// We don't need to clone c here because we're just moving cells to
+			// the left.
+			// b.lines[y][i] = b.lines[y][i+n]
+			b.Lines[y][i] = b.Lines[y][i+n]
+		}
+	}
+
+	// Fill the vacated positions with the given cell
+	for i := area.Max.X - n; i < area.Max.X; i++ {
+		b.SetCell(i, y, c)
 	}
 }
 
