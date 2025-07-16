ffed19981d2289ce84c416964128d285eb0313dd
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-07-16 13:08:11 -0400

    docs: update package description

--- doc.go
+++ doc.go
@@ -1,6 +1,3 @@
-// Package uv (Ultra Violet) is a library for creating terminal-based user
-// interfaces in Go. It provides a high level API for building interactive
-// applications that can run in a terminal window. The library is designed to
-// be easy to use and flexible, allowing developers to create complex UIs with
-// minimal effort.
+// Package uv (Ultraviolet) is a library for creating terminal-based user
+// interfaces in Go.
 package uv
