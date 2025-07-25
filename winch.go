b799ada60ffdf555ed8e32c38f8f4edafa14e6dc
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-07-25 15:57:15 -0400

    refactor: Remove WinChReceiver and related code

--- winch.go
+++ winch.go
@@ -2,7 +2,6 @@ package uv
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"sync"
 
@@ -84,25 +83,4 @@ func (n *WindowSizeNotifier) GetSize() (width, height int, err error) {
 	}
 
 	return width, height, nil
-}
-
-// WinChReceiver listens for window size changes using (SIGWINCH) and sends the
-// new size to the event channel.
-// This is a Unix-specific implementation and should be used on Unix-like
-// systems and won't work on Windows.
-type WinChReceiver struct{ term.File }
-
-// Start starts the receiver.
-func (l *WinChReceiver) Start() error {
-	if l.File == nil {
-		return fmt.Errorf("no file set")
-	}
-	_, _, err := term.GetSize(l.Fd())
-	return err //nolint:wrapcheck
-}
-
-// ReceiveEvents listens for window size changes and sends the new size to the
-// event channel. It stops when the context is done or an error occurs.
-func (l *WinChReceiver) ReceiveEvents(ctx context.Context, evch chan<- Event) error {
-	return l.receiveEvents(ctx, l.File, evch)
 }
