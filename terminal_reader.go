041fe81cd9f5b34886690464970e910560aa10b3
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-08-14 17:10:41 -0400

    docs: remove misleading comment in terminal_reader.go

--- terminal_reader.go
+++ terminal_reader.go
@@ -250,7 +250,6 @@ func (d *TerminalReader) scanEvents(buf []byte, expired bool) (total int, events
 				switch event := event.(type) {
 				case KeyPressEvent:
 					if len(event.Text) > 0 {
-						// We only allow text events in paste mode.
 						d.paste = append(d.paste, event.Text...)
 					} else {
 						d.paste = append(d.paste, buf[:n]...)
