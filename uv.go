25e48a64dfe43a4715c9854103b8fdcd5f83350f
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-09-10 18:14:17 -0400

    chore: add cursor shape tests

--- uv.go
+++ uv.go
@@ -33,24 +33,3 @@ type Screen interface {
 	// WidthMethod returns the width method used by the screen.
 	WidthMethod() WidthMethod
 }
-
-// CursorShape represents a terminal cursor shape.
-type CursorShape int
-
-// Cursor shapes.
-const (
-	CursorBlock CursorShape = iota
-	CursorUnderline
-	CursorBar
-)
-
-// Encode returns the encoded value for the cursor shape.
-func (s CursorShape) Encode(blink bool) int {
-	// We're using the ANSI escape sequence values for cursor styles.
-	// We need to map both [style] and [steady] to the correct value.
-	s = (s * 2) + 1 //nolint:mnd
-	if !blink {
-		s++
-	}
-	return int(s)
-}
