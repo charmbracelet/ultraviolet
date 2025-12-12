charmbracelet/ultraviolet

commit abf0c3f8a42235e7b5ff11677102cd0ec4380189

fix(styled): use Screen's WidthMethod and treat NL as CR-LF

styled.go
@@ -54,8 +54,7 @@ func (s *StyledString) Draw(buf Screen, area Rectangle) {
 	// We need to normalize newlines "\n" to "\r\n" to emulate a raw terminal
 	// output.
 	str = strings.ReplaceAll(str, "\r\n", "\n")
-	str = strings.ReplaceAll(str, "\n", "\r\n")
-	printString(buf, ansi.GraphemeWidth, area.Min.X, area.Min.Y, area, str, !s.Wrap, s.Tail)
+	printString(buf, buf.WidthMethod(), area.Min.X, area.Min.Y, area, str, !s.Wrap, s.Tail)
 }
 
 // Height returns the number of lines in the styled string. This is the number
@@ -102,7 +101,7 @@ var parserPool = &sync.Pool{
 // printString draws a string starting at the given position.
 func printString[T []byte | string](
 	s Screen,
-	m ansi.Method,
+	m WidthMethod,
 	x, y int,
 	bounds Rectangle, str T,
 	truncate bool, tail string,
@@ -174,6 +173,8 @@ func printString[T []byte | string](
 				ReadLink(p.Data(), &link)
 			case ansi.Equal(seq, T("\n")):
 				y++
+				// Always treat a NL as CR-LF similar to Termios ONLCR.
+				fallthrough
 			case ansi.Equal(seq, T("\r")):
 				x = bounds.Min.X
 			default:

commit 6e94e89698aa0daac02d5d2c15f6e2cd588339a7

fix(renderer): use ansi.GraphemeWidth for string drawing
printString() switches the decoder according to the WidthMethod. Using
DecodeSequenceWc() may introduce inconsistency under differnt LANG environment
variables.

Fixes: abf0c3f8a422 ("fix(styled): use Screen's WidthMethod and treat NL as CR-LF")
Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

styled.go
@@ -54,7 +54,7 @@ func (s *StyledString) Draw(buf Screen, area Rectangle) {
 	// We need to normalize newlines "\n" to "\r\n" to emulate a raw terminal
 	// output.
 	str = strings.ReplaceAll(str, "\r\n", "\n")
-	printString(buf, buf.WidthMethod(), area.Min.X, area.Min.Y, area, str, !s.Wrap, s.Tail)
+	printString(buf, ansi.GraphemeWidth, area.Min.X, area.Min.Y, area, str, !s.Wrap, s.Tail)
 }
 
 // Height returns the number of lines in the styled string. This is the number
@@ -101,7 +101,7 @@ var parserPool = &sync.Pool{
 // printString draws a string starting at the given position.
 func printString[T []byte | string](
 	s Screen,
-	m WidthMethod,
+	m ansi.Method,
 	x, y int,
 	bounds Rectangle, str T,
 	truncate bool, tail string,