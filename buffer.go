d926c2afdc6abe90e154248a8e890d3cd260c448
Ayman Bagabas <ayman.bagabas@gmail.com>
2026-02-12 15:39:48 +0300

    perf(buffer): optimize Clear method by directly setting cells to EmptyCell

--- buffer.go
+++ buffer.go
@@ -374,7 +374,12 @@ func (b *Buffer) FillArea(c *Cell, area Rectangle) {
 
 // Clear clears the buffer with space cells and rectangle.
 func (b *Buffer) Clear() {
-	b.ClearArea(b.Bounds())
+	area := b.Bounds()
+	for y := area.Min.Y; y < area.Max.Y; y++ {
+		for x := area.Min.X; x < area.Max.X; x++ {
+			b.Lines[y][x] = EmptyCell
+		}
+	}
 }
 
 // ClearArea clears the buffer with space cells within the specified
